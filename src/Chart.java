ef26f496ed3fb1d353a436e0b0f03b5915a9e9a3
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-02-29 12:23:26 +0900

    Update, クラス分け。

--- src/Chart.java
+++ src/Chart.java
@@ -8,48 +8,58 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
+
+class PieChart{
+    void en(){
+        try {
+            double num0;
+            double num1;
+            double num2;
+            double num3;
+
+            String str;
+            String str1;
+
+            num0 = 32.3;
+            num1 = 20.2;
+            num2 = 4.6;
+            num3 = 42.3;
+
+            str = "スマートフォンのシェア（2009年）";
+            str1 = "円グラフ";
+
+            ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
+
+            DefaultPieDataset data = new DefaultPieDataset();
+            data.setValue("iPhone", num0);
+            data.setValue("Android", num1);
+            data.setValue("BlackBerry", num2);
+            data.setValue("Windows Mobile", num3);
+
+            // 2D
+            //JFreeChart chart = ChartFactory.createPieChart("スマートフォンのシェア（2009年）",data,true,false,false);
+
+            // 3D
+            JFreeChart chart = ChartFactory.createPieChart3D(str, data, true, false, false);
+
+            BufferedImage image = chart.createBufferedImage(399, 200);
+            ImageIO.write(image, "PNG", new File("piechart.png"));
+
+            JFrame frame = new JFrame(str1);
+            frame.add(new ChartPanel(chart));
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }catch (Exception e){
+            System.err.println("Exception error");
+            e.printStackTrace(System.out);
+        }
+    }
+}
 
 public class Chart {
-    public static void main(String[] args) throws IOException {
-
-        double num1;
-        double num2;
-        double num3;
-        double num4;
-
-        String str;
-        String str2;
-
-        num1 = 32.3;
-        num2 = 20.2;
-        num3 = 4.6;
-        num4 = 42.3;
-
-        str ="スマートフォンのシェア（2010年）";
-        str2 = "円グラフ";
-
-        ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
-
-        DefaultPieDataset data = new DefaultPieDataset();
-        data.setValue("iPhone", num1);
-        data.setValue("Android", num2);
-        data.setValue("BlackBerry", num3);
-        data.setValue("Windows Mobile", num4);
-
-        // 2D
-        //JFreeChart chart = ChartFactory.createPieChart("スマートフォンのシェア（2010年）",data,true,false,false);
-
-        // 3D
-        JFreeChart chart = ChartFactory.createPieChart3D(str, data,true,false,false);
-
-        BufferedImage image = chart.createBufferedImage(400,200);
-        ImageIO.write(image, "PNG", new File("piechart.png"));
-
-        JFrame frame = new JFrame(str2);
-        frame.add(new ChartPanel(chart));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String[] args) {
+        final PieChart obj = new PieChart();
+        obj.en();
     }
 }
